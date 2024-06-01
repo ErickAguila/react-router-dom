02acfd2790e156dd2448c17be78977a32e420d8a
erickaguila <erick.aguila55@gmail.com>
2024-05-31 23:21:34 -0400

    useNavigate

--- src/components/BlogPost.jsx
+++ src/components/BlogPost.jsx
@@ -1,19 +1,26 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { blogdata } from "../data/BlogData";
 
 function BlogPost() {
-    const {slug} = useParams();
+  const { slug } = useParams();
+  const navigate = useNavigate();
 
-    const blogpost = blogdata.find(post => post.slug === slug);
+  const blogpost = blogdata.find((post) => post.slug === slug);
 
-    return (
-        <>
-        <h2>{blogpost.title}</h2>
-        <p>{blogpost.author}</p>
-        <p>{blogpost.content}</p>
-        </>
-    )
+  const returnToBlog = () => {
+    navigate("/blog");
+    // navigate(-1);
+  };
+
+  return (
+    <>
+      <h2>{blogpost.title}</h2>
+      <button onClick={returnToBlog}>Volver al blog</button>
+      <p>{blogpost.author}</p>
+      <p>{blogpost.content}</p>
+    </>
+  );
 }
 
 export default BlogPost;
